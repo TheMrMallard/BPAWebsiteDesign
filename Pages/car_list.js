d4a7e782fd4f502054957add9434430ca3c7ab07
SuperSpaze <jv802155@gmail.com>
2023-01-08 01:52:12 -0500

    Updated car list
    Updated car list through creation of JavaScript and implemented in Car listing page

---
+++ Pages/car_list.js
@@ -0,0 +1,33 @@
+// Imports array from car_array.js which has all information regarding cars
+var carList = new Array();
+var info = "";
+
+// ---------------- Database ----------------
+carList.push(
+    {carId: 1, model: 'Ford', fuelType: "Gasoline", price: 1000, mileage: 12421},
+    {carId: 2, model: 'Chevron', fuelType: "Gasoline", price: 5200, mileage: 5832},
+);
+// ---------------- Database ----------------
+
+// Called when the user presses the search button (still testing)
+function searchList() {
+    document.querySelector('#carList').innerHTML += 1;
+}
+
+// For statement
+carList.forEach(function(carInfo) {
+    // Basically imagine this is typing the code into HTML, but I'm basically building it
+    info += 
+    '<ul>' + '<li>' + 'Car Id: ' + carInfo.carId 
+            + '<ul>' 
+                + '<li>' + 'Model: ' + carInfo.model + '</li>'
+                + '<li>' + 'Fuel Type: ' + carInfo.fuelType + '</li>'
+                + '<li>' + 'Price: ' + carInfo.price + '</li>'
+                + '<li>' + 'Mileage: ' + carInfo.mileage + '</li>'
+            + '</ul>' 
+        + '</li>' 
+    + '</ul>'
+});
+
+document.querySelector('#carList').innerHTML = info;
+
