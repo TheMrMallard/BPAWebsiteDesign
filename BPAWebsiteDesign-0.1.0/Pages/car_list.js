12b8704c1c3a3a552bf02136a46fd747c5af5158
Suzaku17 <92160925+Suzaku17@users.noreply.github.com>
2023-01-18 14:09:59 -0500

    Add files via upload

---
+++ BPAWebsiteDesign-0.1.0/Pages/car_list.js
@@ -0,0 +1,124 @@
+// ---------------- VARIABLES ----------------
+var info = "";
+
+// ----------------- STYLING LISTS ------------------------
+/* Creates a unordered list through coding
+* Basically imagine this is typing the code into HTML, but I'm basically building it
+* Note to John (typed by John): Add the bootstrap syntax after layout is planned
+*/
+
+// Builds a profile of a car
+function carInfoBuilder(array) {
+    let builder = ''
+    builder += 
+    // Basically pretend this is building HTML code
+    '<ul>' + '<li>' + 'Car Id: ' + array.carId 
+            + '<ul>' 
+                + '<li>' + 'Model: ' + array.model + '</li>'
+                + '<li>' + 'Fuel Type: ' + array.fuelType + '</li>'
+                + '<li>' + 'Price: ' + array.price + '</li>'
+                + '<li>' + 'Mileage: ' + array.mileage + '</li>'
+
+                + '<li>' + 'Color: ' + array.exteriorColor + '</li>'
+            + '</ul>' 
+        + '</li>' 
+    + '</ul>'
+
+    return builder;
+}
+
+// Step 1 is called when the user presses the search button 
+function searchList() {
+    let keyInput = document.querySelector('#searchInput').value;
+    let words = keyInput.split(' ')
+    
+    document.querySelector('#carList').innerHTML = searchFilter(carList, words);
+    // document.querySelector('#carList').innerHTML += words.length;
+    // document.querySelector('#carList').innerHTML += words.length;
+}
+
+// Step 2 - Filter results based on user input without case sensitivity
+function searchFilter(array, keyValues) {
+    let emptyScore = 0;
+    let arrayScore = []
+    let filteredForm = '';
+    
+
+    // Checks if the user did not type anything (including just spaces), it will restore to its default
+    for(i = 0; i < keyValues.length; i++) {
+        if (keyValues[i] != '') {emptyScore += 1}
+    }
+    if (emptyScore == 0) {return info}
+
+    // Sets the needed amount of indexes to start the algorithm
+    for(i = 0; i < array.length; i++) {
+        arrayScore[i] = 0;
+    }
+
+    // Iterate through each word, and add one point to each word matched in each of the objects
+    for(w = 0; w < keyValues.length; w++) {
+        
+        // Access each value in each object
+        for(i = 0; i < array.length; i++) {
+
+            for (let value in array[i]) {
+                // Will split up words from the value in each object
+                let arrayValue = array[i][value].toString()
+                let valueWords = arrayValue.split(' ')
+
+                let input = keyValues[w].toString()
+
+                // The word will iterate through each word in the input
+                for(j = 0; j < valueWords.length; j++) {
+                    // If values match, add one point to the object
+                    if (valueWords[j].toLowerCase() == input.toLowerCase()) {
+                        arrayScore[i] += 1
+                    }
+                }
+            }
+        }
+    }
+
+    // I want to add an algorithm here that further sorts the cars through a dropdown bar, eg price or name, etc
+    let sortingScore = sortList(array, arrayScore)
+    let score = Math.max(...arrayScore)
+    
+    // Once the sorting iterations are done, check if there is a sort by used and process them in website
+    if (sortBy == 0) {
+        for (i = score; i > 0; i--) {
+            for (j = 0; j < array.length; j++) {
+                // Access object points  
+                if (arrayScore[j] == i) {
+                    filteredForm += carInfoBuilder(array[j])
+                }
+            }
+        }
+
+    } else {
+        for (i = 0; i < sortingScore.length; i++) {
+            // Access object points
+            let position = sortingScore[i][1]
+            filteredForm += carInfoBuilder(array[position])
+        }
+    }
+    return filteredForm;
+}
+
+// Step 3 - check for any duplicates (wip)
+function checkForDuplication(array) {
+    for (i = 0; i < array.length; i++) {
+        if (array.indexOf(array[i]) != array.lastIndexOf(array[i])) {
+            array.splice(i, i-1);
+        }
+    }
+
+    return array;
+}
+
+// Setup the main page showing all of the cars
+carList.forEach(function(carInfo) {
+    info += carInfoBuilder(carInfo)
+});
+
+document.querySelector('#carList').innerHTML = info;
+
---
+++ BPAWebsiteDesign-0.1.0/Pages/sorting_functions.js
@@ -0,0 +1,82 @@
+var sortBy = 0;
+
+function setSort(number) {
+    sortBy = number;
+}
+
+function sortValues(array, arrayScore, keyValue, beReversed) {
+    // Setup valueAndPosition array
+    let valueScorePosition = new Array(arrayScore.length)
+
+    for(i = 0; i < valueScorePosition.length; i++) {
+        valueScorePosition[i] = new Array(3)
+    }
+
+    // Setup other variables
+    let reverse = beReversed;
+    let highest = Math.max(...arrayScore)
+
+   
+    // Iterate through and make valueAndPosition array
+    for(i = 0; i < valueScorePosition.length; i++) {
+        for(j = 0; j < valueScorePosition[i].length; j++) {
+            if(j == 0) {
+                // Set value
+                valueScorePosition[i][j] = array[i][keyValue];
+            } else if (j == 1) {
+                // Set position
+                valueScorePosition[i][j] = i
+            } else {
+                // Set arrayScore
+                valueScorePosition[i][j] = arrayScore[i];
+            }
+        }
+    }
+
+    // Sort valueScorePosition by value (high to low or low to high depending on paramater given)
+    if (reverse) {
+        valueScorePosition.sort((a, b) => (b[0] - a[0] || (b[1] - a[1])))
+    } else {
+        valueScorePosition.sort((a, b) => (a[0] - b[0] || (a[1] - b[1])))
+    }
+
+    let newArrayScore = new Array()
+
+    // Sort the new array by finding scores and putting the scores first as the values are already sorted
+    for(i = highest; i > 0; i--) {
+        for(j = 0; j < valueScorePosition.length; j++) {
+            if(valueScorePosition[j][2] == i) {
+                newArrayScore.push(valueScorePosition[j])
+            }
+        }
+    }
+
+    return newArrayScore;
+}
+
+function sortList(array, arrayScore) {
+    let result = []
+
+    switch(sortBy) {
+        case 0: // default
+            result = arrayScore;
+        break;
+
+        case 1: // price (high to low)
+            result.push(...sortValues(array, arrayScore, 'price', true))
+        break;
+        
+        case 2: // price (low to high)
+            result.push(...sortValues(array, arrayScore, 'price', false))
+        break;
+    }
+
+    return result;
+}
+
+function countDuplicates(array, key) {
+    array.filter((value) => {return value === key})
+}
+function priceHightoLow(array, arrayScore) {
+
+}
