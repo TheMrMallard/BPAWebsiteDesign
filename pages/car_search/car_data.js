23f4f2d4ffe097d4eb894f548bf3277f4e819498
MrMallarb <jv802155@gmail.com>
2023-03-01 01:53:48 -0500

    Tried making id localStorage?

--- pages/car_search/car_data.js
+++ pages/car_search/car_data.js
@@ -1,14 +1,50 @@
-// Create array
+// Create object based array
 var carList = new Array();
 carList.push(
-    {carId: generateId(), model: 'White Land Rover Ranger Rover', fuelType: "Gasoline", price: 87990, mileage: 23421, exteriorColor: 'White', img: '/images/car1.jpeg', alt: 'A car of a white land Rover Ranger Rover suv on the road'},
-    {carId: generateId(), model: 'Blue Mercedes Benz 5', fuelType: "Gasoline", price: 27990, mileage: 12832, exteriorColor: 'Blue', img: '/images/car2.webp', alt: 'A car of a blue Mercedes Benz front-side view looking right'},
-    {carId: generateId(), model: 'Grey Ford Focus Hatchback', fuelType: "Gasoline", price: 15895, mileage: 12421, exteriorColor: 'Grey', img: '/images/car3.jpeg', alt: 'A car of a grey Ford front-side view looking left'},
-    {carId: generateId(), model: 'Blue Audi RS 6', fuelType: "Gasoline", price: 123590, mileage: 3832, exteriorColor: 'Blue', img: '/images/car4.jpeg', alt: 'A car of a blue Audi on its back-side'},
-    {carId: generateId(), model: 'KIA', fuelType: "Electric", price: 17020, mileage: 23421, exteriorColor: 'White', img: '/images/car5.jpeg', alt: 'A car of a white KIA that has all of the doors open with the back as well'},
-    {carId: generateId(), model: 'White Ford Thunderbird', fuelType: "Gasoline", price: 23995, mileage: 12232, exteriorColor: 'White', img: '/images/car6.jpeg', alt: 'A car of a white Ford Thunderbird that is parked outside a building facing right'},
-    {carId: generateId(), model: 'Ford', fuelType: "Electric", price: 24912, mileage: 23232, exteriorColor: 'Black', img: '/images/car7.webp', alt: 'A car of a black Ford looking front-side looking right'},
+    {carId: getId(0), model: 'White Land Rover Ranger Rover', fuelType: "Gasoline", price: 87990, mileage: 23421, exteriorColor: 'White', img: '/images/car1.jpeg', alt: 'A car of a white land Rover Ranger Rover suv on the road'},
+    {carId: getId(1), model: 'Blue Mercedes Benz 5', fuelType: "Gasoline", price: 27990, mileage: 12832, exteriorColor: 'Blue', img: '/images/car2.webp', alt: 'A car of a blue Mercedes Benz front-side view looking right'},
+    {carId: getId(2), model: 'Grey Ford Focus Hatchback', fuelType: "Gasoline", price: 15895, mileage: 12421, exteriorColor: 'Grey', img: '/images/car3.jpeg', alt: 'A car of a grey Ford front-side view looking left'},
+    {carId: getId(3), model: 'Blue Audi RS 6', fuelType: "Gasoline", price: 123590, mileage: 3832, exteriorColor: 'Blue', img: '/images/car4.jpeg', alt: 'A car of a blue Audi on its back-side'},
+    {carId: getId(4), model: 'KIA', fuelType: "Electric", price: 17020, mileage: 23421, exteriorColor: 'White', img: '/images/car5.jpeg', alt: 'A car of a white KIA that has all of the doors open with the back as well'},
+    {carId: getId(5), model: 'White Ford Thunderbird', fuelType: "Gasoline", price: 23995, mileage: 12232, exteriorColor: 'White', img: '/images/car6.jpeg', alt: 'A car of a white Ford Thunderbird that is parked outside a building facing right'},
+    {carId: getId(6), model: 'Ford', fuelType: "Electric", price: 24912, mileage: 23232, exteriorColor: 'Black', img: '/images/car7.webp', alt: 'A car of a black Ford looking front-side looking right'},
 );
+
+function getId(index) {
+    // Iterate through the array and check if carId already has an id, if so get the id, if not, generate one and store it in localStorage
+    var sampleArray = new Array(carList.length)
+
+    // Get the items from the localStorage to the sampleArray
+    var localIds = JSON.parse(localStorage.getItem('ids'))
+
+    if(localIds == null) {
+        sampleArray[index] = generateId()
+
+        localStorage.setItem('ids', JSON.stringify(sampleArray))
+    }
+
+    // Store id from localStorage
+
+    // return generateId()
+    /*
+    // Whats taken in: the item number
+    for(i = 0; i < carList.length; i++) {
+        // Create the id
+        var id = generateId();
+
+        // Get the key and the value (which is an array) from LocalStorage, returns null if no key value pair is found
+
+        // Check if the id is already stored and returns the id stored in localStorage
+        if() {
+
+        } else {
+            // Else, store the generated id and return the id itself
+            
+            return id;
+        }
+    }
+    */
+}
 
 function generateId() {
     let randomLetter = String.fromCharCode(65 + Math.floor(Math.random() * 26))
